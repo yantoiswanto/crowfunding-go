f0b1c0c8097638f474e2c5597721733191f86f3f
Yanto Iswanto <yantoiswanto18@gmail.com>
2023-08-27 10:37:52 +0700

    Campaign Service : getCampaign

--- services/campaign.service.go
+++ services/campaign.service.go
@@ -1,1 +1,36 @@
 package services
+
+import (
+	"crowfunding/models"
+	"crowfunding/repositories"
+)
+
+type CampaignService interface {
+	FindCampaigns(userID int) ([]models.Campaign, error)
+}
+
+type campaignService struct {
+	repository repositories.CampaignRepository
+}
+
+func NewServiceCampaign(campaignRepository repositories.CampaignRepository) *campaignService {
+	return &campaignService{campaignRepository}
+}
+
+func (s *campaignService) FindCampaigns(userID int) ([]models.Campaign, error) {
+	if userID != 0 {
+		campaigns, err := s.repository.FindByUserID(userID)
+		if err != nil {
+			return campaigns, err
+		}
+
+		return campaigns, nil
+	}
+
+	campaigns, err := s.repository.FindAll()
+	if err != nil {
+		return campaigns, err
+	}
+
+	return campaigns, nil
+}
